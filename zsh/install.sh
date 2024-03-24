75e774f26c025440a83ed8f16662eb17652331d5
True Merrill <true.merrill@gmail.com>
2024-03-24 16:28:52 -0400

    Work in progress with ZSH rework

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Install ohmyzsh
+# Install ohmyzsh while still loading DOTFILES
 
 source "$DOTFILES_LIB"
 
@@ -11,4 +11,16 @@ if [ ! -d $OH_MY_ZSH ]; then
   sh -c "$(curl -fsSL $OH_MY_ZSH_URL)"
 fi
 
+# Update zshrc.symlink to set the DOTFILES variable to the value provided
+# during installation.  After install, this allows us to always reference
+# the DOTFILES folder.
+
+if [ ! -f "$DOTFILES/zsh/zshrc.symlink" ]; then
+
+  ESCAPED_DOTFILES=$(printf '%s\n' "$DOTFILES" | sed 's/[\/&]/\\&/g')
+  SUBSTITUTE="s/export DOTFILES=\"\"/export DOTFILES=\"$ESCAPED_DOTFILES\"/"
+
+  cp "$DOTFILES/zsh/zshrc" "$DOTFILES/zsh/zshrc.symlink"
+  sed -i '' "$SUBSTITUTE" "$DOTFILES/zsh/zshrc.symlink"
+fi
 
